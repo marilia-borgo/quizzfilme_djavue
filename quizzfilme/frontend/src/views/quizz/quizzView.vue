f5d24c75607eb0e7a3b0322957b1156e6e07c758
marilia-borgo <marilia.bm01@gmail.com>
2023-01-31 16:10:27 -0300

    value não pode ser o mesmo

--- quizzfilme/frontend/src/views/quizz/quizzView.vue
+++ quizzfilme/frontend/src/views/quizz/quizzView.vue
@@ -3,19 +3,19 @@
         <p> Qual sua cor favorita?</p>
         <v-radio-group v-model="cor">
             <v-radio label="Vermelho" value="a"></v-radio>
-            <v-radio label="rosa" value="b"></v-radio>
-            <v-radio label="preto" value="a"></v-radio>
+            <v-radio label="rosa" value="b1"></v-radio>
+            <v-radio label="preto" value="a1"></v-radio>
             <v-radio label="verde" value="c"></v-radio>
-            <v-radio label="roxo" value="c"></v-radio>
+            <v-radio label="roxo" value="c1"></v-radio>
             <v-radio label="laranja" value="b"></v-radio>
         </v-radio-group>
 
         <p> Rapido! pose pra a foto!</p>
         <v-radio-group v-model="foto">
-            <v-radio label="Hang Loose" value="c"></v-radio>
+            <v-radio label="Hang Loose" value="c1"></v-radio>
             <v-radio label="Coloca a língua pra fora" value="a"></v-radio>
-            <v-radio label="sorri" value="b"></v-radio>
-            <v-radio label="Não faz nada" value="a"></v-radio>
+            <v-radio label="sorri" value="b1"></v-radio>
+            <v-radio label="Não faz nada" value="a1"></v-radio>
             <v-radio label="peace fingers" value="b"></v-radio>
             <v-radio label="congela" value="c"></v-radio>
         </v-radio-group>
@@ -23,31 +23,31 @@
         <p> Qual seu animal favorito?</p>
         <v-radio-group v-model="animal">
             <v-radio label="cobra" value="a"></v-radio>
-            <v-radio label="coelho" value="b"></v-radio>
+            <v-radio label="coelho" value="b1"></v-radio>
             <v-radio label="hamster" value="b"></v-radio>
-            <v-radio label="gato" value="a"></v-radio>
+            <v-radio label="gato" value="a1"></v-radio>
             <v-radio label="sapo" value="c"></v-radio>
-            <v-radio label="cachorro" value="c"></v-radio>
+            <v-radio label="cachorro" value="c1"></v-radio>
         </v-radio-group>
 
         <p> Como você mataria alguém?</p>
         <v-radio-group v-model="mataria">
             <v-radio label="Enforcando" value="a"></v-radio>
-            <v-radio label="Atirando" value="b"></v-radio>
+            <v-radio label="Atirando" value="b1"></v-radio>
             <v-radio label="Nunca faria isso" value="b"></v-radio>
             <v-radio label="Empurrando de um lugar alto" value="c"></v-radio>
-            <v-radio label="Atropelando" value="c"></v-radio>
-            <v-radio label="Esfaqueando" value="a"></v-radio>
+            <v-radio label="Atropelando" value="c1"></v-radio>
+            <v-radio label="Esfaqueando" value="a1"></v-radio>
         </v-radio-group>
 
         <p> Qual sua bebida favorita?</p>
         <v-radio-group v-model="bebida">
-            <v-radio label="leite" value="b"></v-radio>
+            <v-radio label="leite" value="b1"></v-radio>
             <v-radio label="suco" value="b"></v-radio>
-            <v-radio label="cerveja" value="c"></v-radio>
+            <v-radio label="cerveja" value="c1"></v-radio>
             <v-radio label="coca-cola" value="c"></v-radio>
             <v-radio label="café" value="a"></v-radio>
-            <v-radio label="vinho" value="a"></v-radio>
+            <v-radio label="vinho" value="a1"></v-radio>
         </v-radio-group>
         <v-btn @click="contabilizarRespostas()">
             Submit
