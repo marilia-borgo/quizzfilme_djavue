b0c8f2f37daf1f5c616b29c4eb79c8b78ac536ea
marilia-borgo <marilia.bm01@gmail.com>
2023-02-02 17:07:18 -0300

    Adiciona o componente para mostrar os filmes

--- quizzfilme/frontend/src/views/quizz/quizzResult.vue
+++ quizzfilme/frontend/src/views/quizz/quizzResult.vue
@@ -1,12 +1,13 @@
 <template>
+  <div>
   <v-card
-    class="mx-auto"
+    class="mx-auto mt-10"
     max-width="900"
   >
     <v-img
       class="align-end text-white"
       height="200"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      src="https://media.istockphoto.com/id/1350788409/photo/pink-color-background.jpg?b=1&s=170667a&w=0&k=20&c=w2q6NV8dHpNIWpYpq_capQau6-RDNoJepXXMhRH5ySo="
       cover
     >
       <v-card-title>{{resultado.resultado}}</v-card-title>
@@ -27,12 +28,28 @@
         Share
       </v-btn>
 
-      <v-btn color="orange">
+      <v-btn color="orange" @click="showfilmes()">
         Ver filmes
       </v-btn>
     </v-card-actions>
+    <v-window
+    v-if="show"
+    v-model="window"
+    show-arrows
+  >
+    <v-window-item
+      v-for="n in length"
+      :key="n"
+    >
+      <v-card height="200px" class="d-flex justify-center align-center">
+        <span class="text-h2">Card {{ n }}</span>
+      </v-card>
+    </v-window-item>
+  </v-window>
   </v-card>
 
+</div>
+  
 </template>
   
 <script>
@@ -44,10 +61,9 @@ export default {
 data () {
     return {
     show: false,
-    horario: '',
-    planta: '',
-    bebida: '',
-    resultado: ''
+    resultado: '',
+    length: 3,
+    window: 0,
     }
 },
 computed: {
@@ -65,6 +81,13 @@ mounted() {
         this.loading = false
       })
     },
+    showfilmes(){
+      if (this.show === true){
+        this.show = false
+      }else{
+        this.show=true
+      }
+    }
     }
 }
 </script>
