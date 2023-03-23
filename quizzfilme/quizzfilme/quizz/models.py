2032047868d26424e86443f101c40c57291532bc
marcela <marcela.lima@buser.com.br>
2023-03-23 16:28:37 -0300

    salva avaliações

--- quizzfilme/quizzfilme/quizz/models.py
+++ quizzfilme/quizzfilme/quizz/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models import Avg
 
 class Personalidade(models.Model):
     resultado = models.CharField(max_length=512)
@@ -11,4 +12,21 @@ class Personalidade(models.Model):
             'resultado': self.resultado,
         }
 
-# Create your models here.
+class Movie(models.Model):
+    header = models.CharField(max_length=100, default="Header")
+    text = models.TextField()
+
+    def average_rating(self) -> float:
+        return Rating.objects.filter(movie=self).aggregate(Avg("rating"))["rating__avg"] or 0
+
+    def __str__(self):
+        return f"{self.header}: {self.average_rating()}"
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.movie.header}: {self.rating}"
