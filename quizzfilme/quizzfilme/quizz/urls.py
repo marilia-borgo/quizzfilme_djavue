8b6ba8e3b9afb2bf3727b2c2bf209de04de87df3
marcela <marcela.lima@buser.com.br>
2023-03-23 16:43:25 -0300

    feat: salva avaliação por estrelas

--- quizzfilme/quizzfilme/quizz/urls.py
+++ quizzfilme/quizzfilme/quizz/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("add", views.recebe_resultado),
     path("", views.get_resultado),
+    path('rate/<int:post_id>/<int:rating>/', views.rate),
+    path('resultado/', views.save_note),
 ]
--- quizzfilme/quizzfilme/quizz/views.py
+++ quizzfilme/quizzfilme/quizz/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.http import JsonResponse
-
+from django.contrib.auth.models import User
 from .service import resultado
 
 
@@ -16,3 +16,16 @@ def get_resultado(request):
     user = request.user.id
     resultado_filmes = resultado.pega_resultado(user)
     return JsonResponse(resultado_filmes)
+
+def save_note(request: HttpRequest) -> HttpResponse:
+    movies = Movie.objects.all()
+    for movie in movies:
+        rating = Rating.objects.filter(movie=movie, user=request.user).first()
+        movie.user_rating = rating.rating if rating else 0
+    return render(request, "index.html", {"movies": movies})
+
+def rate(request: HttpRequest, movie_id: int, rating: int) -> HttpResponse:
+    movie = Movie.objects.get(id=movie_id)
+    Rating.objects.filter(movie=movie, user=request.user).delete()
+    movie.rating_set.create(user=request.user, rating=rating)
+    return index(request)
